cc12e95c1eef99b8e9490c4089bb5f3cfa446bf7
SeniorShohmalik <93908246+SeniorShohmalik@users.noreply.github.com>
2023-12-13 19:34:49 +0500

    Update settings.py

--- khadis/khadis/settings.py
+++ khadis/khadis/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#$lh&#+jzx-ms98gp0!g7cpv^pw8oq+-!rg&_zrh$y)9z9ke*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
